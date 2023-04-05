88224ebd73aa3d9ea75b7374b468929ab6faa1c9
MikeJWms <michaelwilliams.be@gmail.com>
2023-04-05 12:18:11 -0700

    add serverless functions

---
+++ api/handler.ts
@@ -0,0 +1,18 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { sumTwoNumbers } from 'server/helpers';
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  // use some helpers
+  const sum = sumTwoNumbers(1, 2);
+  console.log(sum)
+
+  // send a response
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
---
+++ server/helpers.ts
@@ -0,0 +1,4 @@
+// return sum of two numbers
+export function sumTwoNumbers(a: number, b: number) {
+  return a + b;
+}
